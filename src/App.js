0fce8b31e19df0605219aae6c327f6f6800b0f36
Sudhendra <contactwithsid@gmail.com>
2023-11-30 05:53:59 +0530

    Added Admin creation API call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+const Login = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+
+  const submitHandler = async (event) => {
+    event.preventDefault()
+
+    const userObj = {
+      name,
+      email,
+      password
+    }
+
+    const response = await fetch('http://localhost:8000', {
+      method: 'POST',
+      body: JSON.stringify(userObj),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const parsedResponse = await response.json()
+
+    console.log("The parsed response", parsedResponse)
+
+  }
+
+  const setNameHandler = event => {
+    setName(event.target.value)
+  }
+
+  const setEmailHandler = event => {
+    setEmail(event.target.value)
+  }
+
+  const setPasswordHandler = event => {
+    setPassword(event.target.value)
+  }
+
+  return <div>
+    <h1> Admin Login/Signup </h1>
+    <form onSubmit={submitHandler}>
+      Email:- <input type="email" value={email} onChange={setEmailHandler} /><br/>
+      Name:- <input type="text" value={name} onChange={setNameHandler} /> <br/>
+      Password:- <input type="password" value={password} onChange={setPasswordHandler} /><br/>
+      <button type="submit" > Submit </button>
+
+    </form>
+  </div>
+
 }
 
-export default App;
+export default Login
